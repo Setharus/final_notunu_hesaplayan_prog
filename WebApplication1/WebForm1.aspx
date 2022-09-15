d82cb6a034127d8afde637661623afde0d1a0dd4
Serhat Oğuz <sserhatoguz@outlook.com>
2022-09-15 20:22:31 +0300

    Add project files.

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="WebForm1.aspx.vb" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Vize Notu"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Hesapla" />
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Alması Gereken Final Notu :"></asp:Label>
+            <asp:Label ID="Label3" runat="server" Text="   . . . . . . . . . "></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.vb
@@ -0,0 +1,24 @@
+﻿Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Vize notu girilen öğrencinin ortalaması 55 üzerinde olacak şekilde
+        ' final sınavından kaç puan alırsa geçeceğini görüntüleyen program //
+        Dim vize, x, ort As Integer
+        vize = TextBox1.Text
+        x = 1
+        ort = vize * 0.4 + x * 0.6
+        If ort >= 55 Then
+            Label3.Text = Convert.ToInt32(x)
+        ElseIf ort < 55 Then
+            For i = ort To 55
+                x += 1
+            Next
+            Label3.Text = Convert.ToInt32((x * 100) / 60)
+        End If
+    End Sub
+End Class
